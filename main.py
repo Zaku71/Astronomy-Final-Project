ae61f4a85f22663de069b314ed39ae53f846ac41
Zaku71 <120686002+Zaku71@users.noreply.github.com>
2022-12-16 20:51:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,87 +26,87 @@ name = input("To begin enter your name: ")
 print("Hello", name, )
 print()
 
-
 while(1):
-    print("please select a location to travel to!")
-    print()
-    for planet in planets:
-        print(planet.upper())
+    while(1):
+        print("please select a location to travel to!")
+        print()
+        for planet in planets:
+            print(planet.upper())
 
-    planet_selection = input().lower()
+        planet_selection = input().lower()
 
-    if planet_selection not in planets:
-        print("Selection Not valid, please enter a selection from the list")
-        continue
+        if planet_selection not in planets:
+            print("Selection Not valid, please enter a selection from the list")
+            continue
 
-    print()
-    print("You chose: " , planet_selection.upper(), " This body is ", planets[planet_selection], "Miles away from Earth\n\n")
-
-    print("Choose your mode of transportation")
-    for v in vehicles:
-        print(v.upper())
-
-    vehicle_selection = input().lower()
-
-    if vehicle_selection not in vehicles:
-        print("Selection Not valid, please enter a selection from the list")
-        continue
-
-    print()
-    print("You chose to go by" ,vehicle_selection.upper(), "travling with a avg speed of", vehicles[vehicle_selection], "MPH")
-    print()
-
-    hours = planets[planet_selection] / vehicles[vehicle_selection]
-
-    d1 = datetime.today()
-    d2 = d1 + timedelta(days = hours / 24.0)
-    
-    days = hours / 24.0
-
-    years = days / 365
-
-    leftover_days = days % 365    
-
-    weeks = leftover_days / 7        
-
-    new_leftover_days = leftover_days % 7  
-
-    not_whole_days = days - int(days)   
-
-    days = int(days)    
-
-    decade = years / 10
-
-    century = decade /10
-
-
-    if century > 1: print("if you leave now it will take you", round(century, 2), "Centuries to get to", planet_selection, "your ETA is", d2)
-
-    elif decade > 1: print("if you leave now it will take you", round(decade, 3), "decades to get to", planet_selection, "your ETA is", d2  )
-
-    elif years > 1:
-        print("if you leave now it will take you", round(years, 3), "years to get to", planet_selection, "your ETA is", d2 )
-
-    else:
-        print("You will make it to", planet_selection, "in", int(years), "years", int(weeks), "weeks", days, "days and", int(hours), "your ETA is", d2 )
-
+        print()
+        print("You chose: " , planet_selection.upper(), " This body is ", planets[planet_selection], "Miles away from Earth\n\n")
+        break
 
     while(1):
-        print()
-        answer = input("Would you like to travle to anoter location?: ").lower()
-        print ()
-        if answer == "yes":
-            
-            break
-        elif answer == "no":
-            print()
-            print("------------------------------------")
-            print(" Thank you for travaling with us!!")
-            print("------------------------------------")
-            print()
-            print()
-            print()
-            exit(1)
-        else: 
-            print("Please type 'Yes' or 'No'")
+        print("Choose your mode of transportation")
+        for v in vehicles:
+            print(v.upper())
 
+        vehicle_selection = input().lower()
+
+        if vehicle_selection not in vehicles:
+            print("Selection Not valid, please enter a selection from the list")
+            continue
+
+        print()
+        print("You chose to go to", planet_selection.upper(), "by" ,vehicle_selection.upper(), "travling with a avg speed of", vehicles[vehicle_selection], "MPH")
+        print()
+
+        hours = planets[planet_selection] / vehicles[vehicle_selection]
+
+        today = datetime.today()
+
+        today_year = today.year
+        
+        days = hours / 24.0
+
+        hours = hours % 24.0
+
+        years = days / 365
+
+        days = days % 365    
+
+        weeks = days / 7        
+
+        days = int(days % 7) 
+
+        century = years /10
+
+        arriaval_year = int(today_year + years)
+
+
+        if century > 1: 
+            print("You will make it to", planet_selection, "in", int(century), "centuries", int(years % 100), "years", int(weeks), "weeks", int(days), "days and", int(hours), "hours, year of arrival is: ", arriaval_year )
+
+        elif years > 1:
+            print("You will make it to", planet_selection, "in", int(years), "years", int(weeks), "weeks", int(days), "days and", int(hours), "hours, your year of arrival is: ", arriaval_year )
+
+        else:
+            print("You will make it to", planet_selection, "in", int(weeks), "weeks", int(days), "days and", int(hours), "hours, your year of arrival is: ", arriaval_year)
+
+
+        while(1):
+            print()
+            answer = input("Would you like to travle to another location?: ").lower()
+            print ()
+            if answer == "yes":
+                
+                break
+            elif answer == "no":
+                print()
+                print("------------------------------------")
+                print(" Thank you for travaling with us!!")
+                print("------------------------------------")
+                print()
+                print()
+                print()
+                exit(1)
+            else: 
+                print("Please type 'Yes' or 'No'")
+        break
